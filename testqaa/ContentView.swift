cf2ffb11be2a723956fd16ee7ddff8039db35f1b
TLtlbo <js1436kt@gmail.com>
2024-05-21 13:14:58 +0900

    Initial Commit

---
+++ testqaa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testqaa
+//
+//  Created by 박진성 on 5/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testqaa/testqaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  testqaaApp.swift
+//  testqaa
+//
+//  Created by 박진성 on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct testqaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
